30271fd092e227eba789d7bb22941677ed067c45
Administrator <Administrator@WIN-14KG1FGK9CB>
2017-01-16 09:02:34 -0500

    Add Files

--- AdvacnedSQL.sql
+++ AdvacnedSQL.sql
@@ -2,10 +2,16 @@
 With Q as
 (Select C.Cust_Gender,Sum(S.Amount_Sold)
 From Sales S Join Customers C on S.Cust_ID = C.Cust_ID
-Group by C.Cust_Gender)
+Group by C.Cust_Gender) 
 Select * from Q;
 -- Using With for Temporary Function
-
-
+With Function To_Percent(p_input number)
+return varchar AS 
+Begin
+return (To_Char(P_input*100,'999.00') || '%');
+End;
+Select To_Percent(0.64) from dual;
+-- Cascade With
+--
 -- Analytic Queries , Connect AS sh user
 
