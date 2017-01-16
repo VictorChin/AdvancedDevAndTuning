1ad493e99455373234674b5366468c50f740f889
Victor Chin <>
2017-01-16 12:56:59 -0500

    Complete Rank + YoYSameMonth

--- Day1.sql
+++ Day1.sql
@@ -18,16 +18,23 @@ Grant Select Any Table To HR;
 --1999   Feb    300  200
 --1999   Mar    500  300
 With MonthlySales AS (
-Select T.Calendar_Year Year, 
-T.CALENDAR_MONTH_NAME Month,
-T.CALENDAR_MONTH_NUMBER MonthNum,
-Sum(Amount_Sold) as Total
-From SH.Sales S Join SH.Times T ON S.Time_ID = T.Time_ID
-Group By T.Calendar_Year, T.CALENDAR_MONTH_NAME,T.CALENDAR_MONTH_NUMBER
-Order By T.Calendar_Year,T.CALENDAR_MONTH_NUMBER)
-Select Year,Month, Total, Round( Avg(Total) over (order by Year,MonthNum)) As RunningAvg
+  Select T.Calendar_Year Year, 
+  T.CALENDAR_MONTH_NAME Month,
+  T.CALENDAR_MONTH_NUMBER MonthNum,
+  Sum(Amount_Sold) as Total
+  From SH.Sales S Join SH.Times T ON S.Time_ID = T.Time_ID
+  Group By T.Calendar_Year, T.CALENDAR_MONTH_NAME,T.CALENDAR_MONTH_NUMBER
+  Order By T.Calendar_Year,T.CALENDAR_MONTH_NUMBER),--<this comma chains another inline view
+ThreeMVAndRank AS (Select Year,Month, Total, 
+Round( Avg(Total) over (Partition By Year order by Year,MonthNum Rows between 2 Preceding AND Current Row)) 
+As RunningAvg,
+Rank() Over (Partition By Year Order By Total DESC) As RankWithInYear,
+MonthNum
 from MonthlySales 
-Order By Year,MonthNum;
+Order By Year,MonthNum)
+Select Year,Month,Total,
+Total - Lag(Total,12) Over (Order By Year,MonthNum) YoYSameMonth from ThreeMVAndRank;
+--Select * from ThreeMVAndRank Where RankWithInYear <=3 Order By 1,5;
 
 
 
